1f3d6357773878a11743e6f24ce131b55627549b
Yoimer <ydrf19@gmail.com>
2017-12-04 23:04:36 -0400

    include and require files

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,29 +1,8 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
-  </head>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-  <body>
-    <header>
-        <h1>GBI Staff Area</h1>
-    </header>
+<div id="content">
+</div>
 
-    <navigation>
-        <ul>
-            <li><a href="index.php">Menu</a></li>
-        </ul>
-    </navigation>
-
-    <div id="content">
-    </div>
-
-    <footer>
-        &copy; <?php echo date('Y'); ?> Globe Bank
-    </footer>
-
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
