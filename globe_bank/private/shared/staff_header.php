e3399a6024ecc5bcfa0dfc7d691eff86149fa450
Yoimer <ydrf19@gmail.com>
2017-12-07 17:32:29 -0400

    links and URLs

--- globe_bank/private/shared/staff_header.php
+++ globe_bank/private/shared/staff_header.php
@@ -11,7 +11,7 @@
   <head>
     <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>"/>
   </head>
 
   <body>
@@ -21,6 +21,6 @@
 
     <navigation>
         <ul>
-            <li><a href="index.php">Menu</a></li>
+            <li><a href="<?php echo url_for(('/staff/index.php')); ?>">Menu</a></li>
         </ul>
     </navigation>
