db96c2d7e090be4b9f0b48add79e53db3077afed
Yoimer <ydrf19@gmail.com>
2017-12-07 21:18:58 -0400

    use URL parameters

---
+++ globe_bank/public/staff/subjects/show.php
@@ -0,0 +1,30 @@
+<?php
+// Values are always strings using super global $_GET
+
+
+//$page = $_GET['page'];
+//echo gettype($page);
+// 'string'
+
+//$page_as_int = (int) $_GET['page'];
+//echo gettype($page_as_int);
+// 'integer'
+?>
+
+<?php
+
+$id = $_GET['id'];
+
+echo $id ;
+
+/*
+
+includes page as parameter in GET query from browser
+
+echo '<br/>';
+
+$page = $_GET['page'];
+
+echo $page;*/
+
+?>
