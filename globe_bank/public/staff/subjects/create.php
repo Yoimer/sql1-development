ee3143042127689e5f160d0d5e97710df6c9cc13
Yoimer <ydrf19@gmail.com>
2017-12-18 22:39:51 -0400

    detect form submission

--- globe_bank/public/staff/subjects/create.php
+++ globe_bank/public/staff/subjects/create.php
@@ -1,15 +1,22 @@
 <?php
 
-// Handle form values sent by new.php
+require_once('../../../private/initialize.php');
 
-$menu_name = isset($_POST['menu_name']) ? $_POST['menu_name'] : 'DEFAULT VALUE';
-$position  = isset($_POST['position']) ? $_POST['position'] : 'DEFAULT VALUE';
-$visible = isset($_POST['visible']) ? $_POST['visible'] : 'DEFAULT VALUE';
+if(is_post_request()) {
 
-echo "Form parameters<br />";
-echo "Menu name: " . $menu_name . "<br />";
-echo "Position: " . $position . "<br />";
-echo "Visible: " . $visible . "<br />";
+    // Handle form values sent by new.php
+
+    $menu_name = isset($_POST['menu_name']) ? $_POST['menu_name'] : 'DEFAULT VALUE';
+    $position  = isset($_POST['position']) ? $_POST['position'] : 'DEFAULT VALUE';
+    $visible = isset($_POST['visible']) ? $_POST['visible'] : 'DEFAULT VALUE';
+
+    echo "Form parameters<br />";
+    echo "Menu name: " . $menu_name . "<br />";
+    echo "Position: " . $position . "<br />";
+    echo "Visible: " . $visible . "<br />";
+}   else {
+    redirect_to(url_for('/staff/subjects/new.php'));
+}
 
 ?>
 
