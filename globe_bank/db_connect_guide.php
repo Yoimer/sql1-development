0c1b3518e6e0ae09530e411bafec0f499e182fdc
Yoimer <ydrf19@gmail.com>
2017-12-26 13:56:08 -0400

    work with retrieved data

--- globe_bank/db_connect_guide.php
+++ globe_bank/db_connect_guide.php
@@ -17,11 +17,13 @@ $query = "SELECT * FROM subjects";
 $result_set = mysqli_query($connection, $query);
 
 // 3. Use returned data (if any)
+while($subject = mysqli_fetch_assoc($result_set)) {
+  echo $subject["menu_name"] . "<br />";
+}
 
 // 4. Release returned data
 mysqli_free_result($result_set);
 
 // 5. Close database connection
 mysqli_close($connection);
-
 ?>
--- globe_bank/public/staff/subjects/index.php
+++ globe_bank/public/staff/subjects/index.php
@@ -2,14 +2,8 @@
 
 <?php
 
+  /* find subjects in database    */
   $subject_set = find_all_subjects();
-
-  $subjects = [
-    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About Globe Bank'],
-    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Consumer'],
-    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Small Business'],
-    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Commercial'],
-  ];
 ?>
 
 <?php $page_title = 'Subjects'; ?>
@@ -34,11 +28,12 @@
                 <th>&nbsp;</th>
             </tr>
 
-            <?php foreach($subjects as $subject) { ?>
+            <!-- while there are values in data base, show them -->
+            <?php while ($subject = mysqli_fetch_assoc($subject_set)) { ?>
                 <tr>
                     <td><?php echo h($subject['id']); ?></td>
                     <td><?php echo h($subject['position']); ?></td>
-                    <td><?php $subject['visible'] == 1 ? 'true' : 'false'; ?></td>
+                    <td><?php echo h($subject['visible'] == 1 ? 'true' : 'false'); ?></td>
                     <td><?php echo h($subject['menu_name']); ?></td>
                     <td><a class="action" href="<?php echo url_for('/staff/subjects/show.php?id=' . h(u($subject['id']))); ?>">View</a></td>
                     <!-- includes 2 parameters -->
