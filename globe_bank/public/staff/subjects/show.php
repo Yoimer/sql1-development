5fbf4e340b29ad76435b0c0f830e7b12c8a954d8
Yoimer <ydrf19@gmail.com>
2017-12-26 17:25:33 -0400

    find a single record

--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -17,19 +17,42 @@
 
 //$id = $_GET['id'] ?? '1'; // PHP > 7.0
 
-
 $id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
 
-echo h($id) ;
-
-/*
-
-includes page as parameter in GET query from browser
-
-echo '<br/>';
-
-$page = $_GET['page'];
-
-echo $page;*/
+$subject = find_subject_by_id($id);
 
 ?>
+
+<?php $page_title = 'Show Subjects'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+    <a class="back-link" href="<?php echo url_for ('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+    <div class="subject show">
+
+      <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+
+      <div class="attributes">
+        <dl>
+          <dt>Menu Name</dt>
+          <dd><?php echo h($subject['menu_name']); ?></dd>
+        </dl>
+
+        <dl>
+          <dt>Position</dt>
+          <dd><?php echo h($subject['position']); ?></dd>
+        </dl>
+
+        <dl>
+          <dt>Visible</dt>
+          <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+        </dl>
+      </div>
+
+    </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
