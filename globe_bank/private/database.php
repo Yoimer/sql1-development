effb79fe8fffc1a6ac69aaabbe9fd20bef07e967
Yoimer <ydrf19@gmail.com>
2017-12-26 14:32:24 -0400

    error handling

--- globe_bank/private/database.php
+++ globe_bank/private/database.php
@@ -4,6 +4,7 @@
 
 	function db_connect() {
 		$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+		confirm_db_connect();
 		return $connection;
 	}
 
@@ -12,4 +13,22 @@
 			mysqli_close($connection);
 		}
 	}
+
+	function confirm_db_connect() {
+		// test if connection succeeded
+		if(mysqli_connect_errno()) {
+  		$msg = "Database connection failed: ";
+  		$msg .= mysqli_connect_error();
+  		$msg .= " (" . mysqli_connect_errno() . ")";
+  		exit($msg);
+		}
+	}
+
+	function confirm_result_set($result_set) {
+		// test if query succeeded
+		if (!$result_set) {
+ 			exit("Database query failed.");
+		}
+	}
+
 ?>
--- globe_bank/private/query_functions.php
+++ globe_bank/private/query_functions.php
@@ -5,7 +5,10 @@
 
     $sql = "SELECT * FROM subjects ";
     $sql .= "ORDER BY position ASC"; // ascending order
+    //uncomment when troubleshooting
+    //echo $sql . "<br>";
     $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
     return $result;
   }
 
