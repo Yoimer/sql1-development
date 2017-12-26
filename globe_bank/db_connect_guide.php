b02e1ff837e120fe84cbdc34e78d171c0998dcb5
Yoimer <ydrf19@gmail.com>
2017-12-26 13:24:06 -0400

    added db_connect_guide.php

--- globe_bank/db_connect_guide.php
+++ globe_bank/db_connect_guide.php
@@ -4,19 +4,22 @@
 // of database interaction using PHP.
 
 // Credentials
-$dbhost = 'localhost';
-$dbuser = 'ragnar';
-$dbpass = 'lothbrok';
-$dbname = 'globe_bank';
+$dbhost = 'localhost'; // localhost or remote IP
+$dbuser = ''; //user
+$dbpass = ''; // password
+$dbname = ''; // database name
 
 // 1. Create a database connection
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // 2. Perform database query
+$query = "SELECT * FROM subjects";
+$result_set = mysqli_query($connection, $query);
 
 // 3. Use returned data (if any)
 
 // 4. Release returned data
+mysqli_free_result($result_set);
 
 // 5. Close database connection
 mysqli_close($connection);
