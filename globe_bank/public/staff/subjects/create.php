97661648ede2dc64a0ff5e8bebb87e8d5a37315a
Yoimer <ydrf19@gmail.com>
2017-12-26 20:55:34 -0400

    use form data to create records

--- globe_bank/public/staff/subjects/create.php
+++ globe_bank/public/staff/subjects/create.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../../../private/initialize.php'); 
+require_once('../../../private/initialize.php');
 
 /* if id is not present, just redirect to index */
 $menu_name = '';
@@ -17,54 +17,12 @@ if(is_post_request()) {
     $position  = isset($_POST['position']) ? $_POST['position'] : 'DEFAULT VALUE';
     $visible = isset($_POST['visible']) ? $_POST['visible'] : 'DEFAULT VALUE';
 
-    echo "Form parameters<br />";
-    echo "Menu name: " . $menu_name . "<br />";
-    echo "Position: " . $position . "<br />";
-    echo "Visible: " . $visible . "<br />";
+    $result = insert_subject($menu_name, $position, $visible);
+    $new_id = mysqli_insert_id($db);
+    redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
+
+} else {
+  redirect_to(url_for('/staff/subjects/new.php'));
 }
 
 ?>
-
-<?php $page_title = 'Create Subject'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
-
-<div id="content">
-
-    <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
-
-    <div class="subject Create">
-        <h1>Create Subject</h1>
-
-        <form action="<?php echo url_for('/staff/subjects/new.php?'); ?>" method="post">
-            <dl>
-                <dt>Menu Name</dt>
-                <dd><input type="text" name="menu_name" value="<?php echo $menu_name; ?>" /></dd>
-            </dl>
-
-            <dl>
-                <dt>Position</dt>
-                <dd>
-                    <select name="position">
-                        <option value="1">1</option>
-                    </select>
-                </dd>
-            </dl>
-
-            <dl>
-                <dt>Visible</dt>
-                <dd>
-                    <input type="hidden" name="visible" value="0" />
-                    <input type="checkbox" name="visible" value="1" />
-                </dd>
-            </dl>
-
-            <div id="operations">
-                <input type="submit" value="Create Subject" />
-            </div>
-        </form>
-
-    </div>
-
-</div>
-
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
--- globe_bank/public/staff/subjects/new.php
+++ globe_bank/public/staff/subjects/new.php
@@ -1,18 +1,7 @@
 <?php
 
-require_once('../../../private/initialize.php'); ?>
+require_once('../../../private/initialize.php');
 
-<?php 
-
-$test = isset($_GET['test']) ? $_GET['test'] : 'DEFAULT VALUE';
-
-if($test == '404') {
-    error_404();
-} elseif($test == '500') {
-    error_500();
-} elseif($test == 'redirect') {
-    redirect_to(url_for('/staff/subjects/index.php'));
-}
 ?>
 
 <?php $page_title = 'Create Subject'; ?>
