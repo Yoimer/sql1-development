f9b2e90b2cc9f8b21d0492e1d063ce6ca1c80954
Yoimer <ydrf19@gmail.com>
2018-01-04 00:21:59 -0400

    added alexa file

---
+++ globe_bank/public/alexa.php
@@ -0,0 +1,21 @@
+<?php
+    // get request using OK, ON or OFF as query parameters
+    //path/alexa.php?id=OK
+    //path/alexa.php?id=ON
+    //path/alexa.php?id=OFF
+    $id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
+    switch ($id) {
+        case 'OK':
+            echo "OK";
+            break;
+        case 'ON':
+            echo "ON";
+            break;
+        case 'OFF':
+            echo "OFF";
+            break;    
+        default:
+            echo "Check string!!";
+            break;
+        }
+?>
